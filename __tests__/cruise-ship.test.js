925d2b311fec7216492737e9ae4f058721c96f81
Ryan Smillie <RyanJSmillie@hotmail.co.uk>
2022-04-11 22:48:47 +0100

    dry

--- __tests__/cruise-ship.test.js
+++ __tests__/cruise-ship.test.js
@@ -2,33 +2,33 @@ const CruiseShip = require('../src/cruise-ship');
 const Port = require('../src/port');
 const Itinerary = require('../src/itinerary');
 
+describe('Ship', () => {
+    let ship;
+    let port1;
+    let port2;
+    let itinerary;
+
+    beforeEach(() => {
+        port1 = new Port('Manchester')
+        port2 = new Port('Liverpool')
+        port3 = new Port('Southampton')
+        itinerary = new Itinerary([port1, port2, port3])
+        ship = new CruiseShip(itinerary);
+    });
+
+
 describe('constructor', () => {
     it('returns an object', () => {
-        const port1 = new Port('Manchester')
-        const port2 = new Port('Liverpool')
-        const port3 = new Port('Southampton')
-        const itinerary = new Itinerary([port1, port2, port3])
-        const ship = new CruiseShip(itinerary);
         expect(new CruiseShip(itinerary)).toBeInstanceOf(Object);
     });
 });
 
 describe('ship passengers', () => {
     it('returns ship passengers', () => {
-        const port1 = new Port('Manchester')
-        const port2 = new Port('Liverpool')
-        const port3 = new Port('Southampton')
-        const itinerary = new Itinerary([port1, port2, port3])
-        const ship = new CruiseShip(itinerary);
         expect(ship.passengers).toEqual(0);
     });
 
     it('returns new number of passengers', () => {
-        const port1 = new Port('Manchester')
-        const port2 = new Port('Liverpool')
-        const port3 = new Port('Southampton')
-        const itinerary = new Itinerary([port1, port2, port3])
-        const ship = new CruiseShip(itinerary);
         ship.addPassengers(4);
 
         expect(ship.passengers).toEqual(4);
@@ -37,11 +37,6 @@ describe('ship passengers', () => {
 
 describe('checks setSail', () => {
     it('can set sail', () => {
-        const port1 = new Port('Manchester')
-        const port2 = new Port('Liverpool')
-        const port3 = new Port('Southampton')
-        const itinerary = new Itinerary([port1, port2, port3])
-        const ship = new CruiseShip(itinerary);
         const startPort = itinerary.destinations[0];
         ship.setSail();
       
@@ -51,34 +46,18 @@ describe('checks setSail', () => {
       });
       
     it('sets previous port to current port', () => {
-        const port1 = new Port('Manchester')
-        const port2 = new Port('Liverpool')
-        const port3 = new Port('Southampton')
-        const itinerary = new Itinerary([port1, port2, port3])
-        const ship = new CruiseShip(itinerary);
         ship.setSail();
 
         expect(ship.previousPort).toEqual(port1);
     });
 
     it('returns if sailing', () => {
-        const port1 = new Port('Manchester')
-        const port2 = new Port('Liverpool')
-        const port3 = new Port('Southampton')
-        const itinerary = new Itinerary([port1, port2, port3])
-        const ship = new CruiseShip(itinerary);
         ship.setSail();
 
         expect(ship.docked).toBeFalsy();
     });
 
     it('can\'t sail further than its itinerary', () => {
-        const port1 = new Port('Manchester')
-        const port2 = new Port('Liverpool')
-        const port3 = new Port('Southampton')
-        const itinerary = new Itinerary([port1, port2, port3])
-        const ship = new CruiseShip(itinerary);
-      
         ship.setSail();
         ship.dock();
         ship.setSail();
@@ -90,30 +69,15 @@ describe('checks setSail', () => {
 
 describe('ships dock status', () => {
     it('returns ship starting port', () => {
-        const port1 = new Port('Manchester')
-        const port2 = new Port('Liverpool')
-        const port3 = new Port('Southampton')
-        const itinerary = new Itinerary([port1, port2, port3])
-        const ship = new CruiseShip(itinerary);
         expect(ship.dockedPort).toEqual(port1);
     });
 
 
     it('initial previous port is null', () => {
-        const port1 = new Port('Manchester')
-        const port2 = new Port('Liverpool')
-        const port3 = new Port('Southampton')
-        const itinerary = new Itinerary([port1, port2, port3])
-        const ship = new CruiseShip(itinerary);
         expect(ship.previousPort).toEqual(null);
     });
 
     it('can dock at a different port', () => {
-        const port1 = new Port('Manchester')
-        const port2 = new Port('Liverpool')
-        const port3 = new Port('Southampton')
-        const itinerary = new Itinerary([port1, port2, port3])
-        const ship = new CruiseShip(itinerary);
         ship.setSail();
         ship.dock();
 
@@ -122,15 +86,9 @@ describe('ships dock status', () => {
     });
 
     it('gets added to port on instantiation', () => {
-        const port1 = new Port('Manchester')
-        const port2 = new Port('Liverpool')
-        const port3 = new Port('Southampton')
-        const itinerary = new Itinerary([port1, port2, port3])
-        const ship = new CruiseShip(itinerary);
-
         expect(port1.ships).toContain(ship);
     });
 });
-
+});
 
 
